431b31e2921f9cf03eef37279bbbf63e63a74a26
camie-ace <116152278+camie-ace@users.noreply.github.com>
2024-04-25 13:12:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ link.setAttribute("rel", "stylesheet");
 link.setAttribute("href", "https://camie-ace.github.io/sidyum-sales-form/btn.css");
 document.head.appendChild(link);
 
-
 const sidy_container_holder = document.querySelector(".sidy-btn-container");
 
 const button = document.createElement("button");
@@ -12,7 +11,7 @@ const button = document.createElement("button");
 button.id = "sidyum-ebeddable-btn";
 button.className = "sidyum-ebeddable-btn sidyum-ebeddable-btn-bg";
 button.textContent = "Start Demo";
-button.setAttribute("onclick","makeSidyumDemoCall()");
+button.setAttribute("onclick", "createConfirmationModal()");
 
 sidy_container_holder.append(button);
 
@@ -30,12 +29,20 @@ const makeSidyumDemoCall = async () => {
 
   const url = new URL(window.location.href);
   const queryParams = new URLSearchParams(url.search);
-  const queryValueName = queryParams.get("Name");
-  const queryValueBusinessName = queryParams.get("businessName");
-  const queryValueNumber = queryParams.get("phoneNumber");
+  const queryValueName = document.getElementById(
+    "sidy-con-firm-modal-nameDisplay"
+  ).value;
+  const queryValueBusinessName = document.getElementById(
+    "sidy-con-firm-modal-businessNameDisplay"
+  ).value;
+  const queryValueNumber = document.getElementById(
+    "sidy-con-firm-modal-phoneNumberDisplay"
+  ).value;
   console.log(queryValueName);
   console.log(queryValueBusinessName);
   console.log(queryValueNumber);
+  document.getElementById("sidyum-ebeddable-btn").innerText =
+    "I'm calling your phone now";
   const result = await fetch("https://ma0358.buildship.run/demo-ai", {
     method: "post",
     headers: {
@@ -44,17 +51,59 @@ const makeSidyumDemoCall = async () => {
     body: JSON.stringify({
       name: queryValueName,
       businessName: queryValueBusinessName,
-      number: `+${queryValueNumber}`,
+      number: `${queryValueNumber}`,
     }),
   });
+  var intervalId = null;
 
   if (result.status === 200 || result.status === 201) {
-    setTimeout(updateSidyBTN, 120000);
+    let answer = await result.json();
+    const current_call_id = answer.id;
+    console.log(current_call_id);
+    intervalId = setInterval(async () => {
+      const call_status_result = await fetch(
+        "https://ma0358.buildship.run/get_call_status",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json", // Set the content type header
+          },
+          body: JSON.stringify({
+            callId: current_call_id,
+          }),
+        }
+      );
+      answer = await call_status_result.json();
+      console.log(answer);
+      if (answer.call_status === "ended") {
+        clearInterval(intervalId);
+        intervalId = null;
+        if (answer.call_answered === "1") {
+          updateSidyBTN();
+          document.getElementById("sidyum-ebeddable-btn").innerText =
+            "It was nice speaking with you";
+        } else {
+          updateSidyBTN();
+          document.getElementById("sidyum-ebeddable-btn").innerText =
+            "No one answered the call";
+        }
+      }
+    }, 20000);
   } else {
     updateSidyBTN();
     const res = await result.json();
     console.log(res);
-    create_sidy_modal(res.message[0]);
+    document.getElementById("sidyum-ebeddable-btn").innerText = "Call Failed";
+    document.getElementById("sidyum-ebeddable-btn").style.color = "red";
+    if (typeof res.message === "string") {
+      create_sidy_modal("Sorry I can't call this number, try another number");
+    } else {
+      create_sidy_modal("Sorry the number provided isn't valid");
+    }
+    setTimeout(() => {
+      document.getElementById("sidyum-ebeddable-btn").innerText = "Start Demo";
+      document.getElementById("sidyum-ebeddable-btn").style.color = "black";
+    }, 5000);
   }
 };
 
@@ -82,7 +131,6 @@ function create_sidy_modal(err_msg) {
   const closeButton = document.createElement("span");
   closeButton.className = "Sidy-close";
   closeButton.innerHTML = "&times;";
-  closeButton.setAttribute("onclick", "del_sidy_modal()");
   // Create the error message
   const errorMessage = document.createElement("p");
   errorMessage.textContent = err_msg;
@@ -91,16 +139,111 @@ function create_sidy_modal(err_msg) {
   modalContent.appendChild(closeButton);
   modalContent.appendChild(errorMessage);
 
+  closeButton.addEventListener("click", () => {
+    modal.remove();
+  });
   // Append the modal content container to the modal container
   modal.appendChild(modalContent);
   document.body.appendChild(modal);
 }
 
-function del_sidy_modal() {
-  var sidy_close_modal_btn = document.getElementsByClassName("Sidy-close")[0];
-  var sidy_close_modal = document.getElementsByClassName("Sidy-modal")[0];
+/* changes to make 
 
-  sidy_close_modal.addEventListener("click", () => {
-    sidy_close_modal.remove();
+pop up to confirm phone number
+validate phone number using regex
+pre populate pop up from url phone number paraemter
+
+*/
+function createConfirmationModal() {
+  // Get the query parameters
+  const url = new URL(window.location.href);
+  const queryParams = new URLSearchParams(url.search);
+  const name = queryParams.get("Name");
+  const businessName = queryParams.get("businessName");
+  const phoneNumber = queryParams.get("phoneNumber");
+
+  // Create the modal elements
+  const modal = document.createElement("div");
+  modal.id = "sidy-con-firm-modal";
+  modal.classList.add("sidy-con-firm-modal");
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("sidy-con-firm-modal-content");
+
+  const span = document.createElement("span");
+  span.classList.add("sidy-con-firm-modal-close");
+  span.innerHTML = "&times;";
+
+  const heading = document.createElement("h2");
+  heading.textContent = "Please confirm your details:";
+
+  const nameDisplay = document.createElement("p");
+  const nameSpan = document.createElement("input");
+  nameSpan.id = "sidy-con-firm-modal-nameDisplay";
+  nameDisplay.innerHTML = "<span>Name: </span>";
+  nameDisplay.appendChild(nameSpan);
+
+  const businessNameDisplay = document.createElement("p");
+  const businessNameSpan = document.createElement("input");
+  businessNameSpan.id = "sidy-con-firm-modal-businessNameDisplay";
+  businessNameDisplay.innerHTML = "<span>Business Name: </span>";
+  businessNameDisplay.appendChild(businessNameSpan);
+
+  const phoneNumberDisplay = document.createElement("p");
+  const phoneNumberSpan = document.createElement("input");
+  phoneNumberSpan.id = "sidy-con-firm-modal-phoneNumberDisplay";
+  phoneNumberDisplay.innerHTML = "<span>Phone Number: </span>";
+  phoneNumberDisplay.appendChild(phoneNumberSpan);
+
+  const confirmButton = document.createElement("button");
+  confirmButton.id = "sidy-con-firm-modal-confirmDetailsButton";
+  confirmButton.textContent = "Confirm";
+
+  // Append elements to the modal
+  modalContent.appendChild(span);
+  modalContent.appendChild(heading);
+  modalContent.appendChild(nameDisplay);
+  modalContent.appendChild(businessNameDisplay);
+  modalContent.appendChild(phoneNumberDisplay);
+  modalContent.appendChild(confirmButton);
+  modal.appendChild(modalContent);
+  document.body.append(modal);
+
+  document.getElementById("sidy-con-firm-modal-nameDisplay").value = name;
+  document.getElementById("sidy-con-firm-modal-businessNameDisplay").value =
+    businessName;
+  document.getElementById("sidy-con-firm-modal-phoneNumberDisplay").value =
+    "+" + phoneNumber;
+
+  // Add event listeners
+  span.addEventListener("click", () => {
+    modal.remove();
   });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.remove();
+    }
+  });
+
+  document
+    .querySelector("#sidy-con-firm-modal-confirmDetailsButton")
+    .addEventListener("click", () => {
+      if (
+        isValidPhoneNumber(
+          document.getElementById("sidy-con-firm-modal-phoneNumberDisplay")
+            .value
+        )
+      ) {
+        makeSidyumDemoCall();
+        document.querySelector(".sidy-con-firm-modal-close").click();
+      } else {
+        create_sidy_modal("Please enter a valid phone number.");
+      }
+    });
+}
+function isValidPhoneNumber(phoneNumber) {
+  phoneNumber = phoneNumber.replace(/\s/g, "");
+  const phoneNumberPattern = /^\+1\d{10}$/;
+  return phoneNumberPattern.test(phoneNumber);
 }
